c0175bf031def2b74d8b773a31ff7997f1beb03d
bwerth <bryan.werth@students.olin.edu>
2016-05-04 17:30:17 -0400

    Minor Changes

--- markov_melody.py
+++ markov_melody.py
@@ -156,7 +156,7 @@ def generate_string_3(note_seed, rhythm_seed,length,note_database,rhythm_databas
     return (note_output,rhythm_output)
 
 def get_dataset(dataset_index=0):
-    path="/home/joseph/InteractiveProgramming-1/data{}".format(dataset_index)
+    path="/home/bwerth/InteractiveProgramming-1/data{}".format(dataset_index)
     scores=[]
     for filename in os.listdir(path):
         scores.append(converter.parse("{}/{}".format(path,filename)))
@@ -246,8 +246,4 @@ def generate_song(length,dataset_index,note_seed,rhythm_seed,markov_order):
     else:
         print "Sorry, that markov order is not an option"
 
-<<<<<<< HEAD
-generate_song(50,0,["C","D","E-"],[2.0,1.0,1.0],3)
-=======
 generate_song(50,0,["C","F","G"],[3.0,1.0,1.0],3)
->>>>>>> 3aaaea8f6d5cb6fcdcc4678b4f6f28c69af952f9
